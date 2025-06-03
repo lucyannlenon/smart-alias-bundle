605a1cbb9bf35576a0b261e6a127c1f4ef2ae270
lucyannl <lucyann.lenon@gmail.com>
2025-06-03 10:45:29 -0300

    feat: initial release of SmartAliasBundle
    - Add support for #[DefaultImplementationFor] to define singleton service bindings
    - Add support for #[ImplementationFor] to tag multiple implementations of an interface
    - Automatically generates service aliases and tags based on attributes
    - Differentiates behavior in dev (regenerate config on each request) and prod (cached config)
    - Throws exception on duplicate DefaultImplementationFor usage
    - Integrates with Symfony's cache:clear for production config
    - Includes PHPUnit test for detecting duplicate singleton bindings

--- src/DependencyInjection/SmartAliasExtension.php
+++ src/DependencyInjection/SmartAliasExtension.php
@@ -3,6 +3,7 @@ namespace LENON\SmartAliasBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\Resource\FileResource;
 
 class SmartAliasExtension extends Extension
 {
@@ -10,7 +11,7 @@ class SmartAliasExtension extends Extension
     {
         $file = $container->getParameter('kernel.project_dir') . '/config/smart_alias/services.php';
         if (file_exists($file)) {
-            $container->addResource($file);
+            $container->addResource(new FileResource($file)); // <- corrigido aqui
             $loader = require $file;
             $loader($container->getConfigurator());
         }
